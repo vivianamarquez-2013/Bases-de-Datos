a03b210f78a19494edc7ba31ea107a060176c359
Viviana Márquez <vivianam.penam@konradlorenz.edu.co>
2015-10-20 11:25:22 -0500

    Create insertardatos1.php

---
+++ insertardatos1.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>Adicionar datos</title>
+</head>
+<body>
+<form>
+	<center><td colspan='2' align='center'><h1><font color="red">Adicionar clientes</h1></font></td></center>
+	<table align='center' width='225' cellspacing='2' cellpadding='2' border='0'>	
+</form>
+<form method='get' action='insertar_datos2.php'>
+	Código del cliente:
+	<input type='text' name='cod_cliente'>
+	Nombre del cliente:
+	<input type='text' name='nom_cliente'>
+	<br>
+	<br>
+	<input type='submit' value='OK'>
+</form>
+</body>
+</html>
