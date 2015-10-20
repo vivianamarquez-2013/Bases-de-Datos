e5ea4c1739461a11f64f7966f55ca470de54aae7
Viviana Márquez <vivianam.penam@konradlorenz.edu.co>
2015-10-20 12:06:23 -0500

    Create insertardatos3.php

---
+++ hw/insertardatos3.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>Adicionar datos</title>
+</head>
+<body>
+<form>
+	<center><td colspan='2' align='center'><h1><font color="hotpink">Adicionar Eventos</h1></font></td></center>
+	<table align='center' width='225' cellspacing='2' cellpadding='2' border='0'>	
+</form>
+<form method='get' action='insertar_datos4.php'>
+	Código del evento:&nbsp;&nbsp;&nbsp;&nbsp;
+	<input type='text' name='cod_evento'>
+	<br><br>
+	Nombre del evento:&nbsp;&nbsp;&nbsp;&nbsp;
+	<input type='text' name='nom_evento'>
+	<br><br>
+	Fecha del evento:&nbsp;&nbsp;&nbsp;&nbsp;
+	<input type='text' name='fecha_evento'>
+	<br><br>
+	Número de participantes:&nbsp;&nbsp;&nbsp;&nbsp;
+	<input type='text' name='num_part_evento'>
+	<br><br>
+	Lugar del evento:&nbsp;&nbsp;&nbsp;&nbsp;
+	<input type='text' name='lug_evento'>
+	<br>
+	<br>
+	<input type='submit' value='OK'>
+</form>
+</body>
+</html>
